2f9736d35539126aee670e478e89fe6a70e8a725
Sanojan Thiyagaraja <sanojan99@gmail.com>
2021-05-28 19:19:54 +1000

    added development environemtn variable

--- pages/api/login.js
+++ pages/api/login.js
@@ -9,17 +9,15 @@ export default async (req, res) => {
   if (req.method !== "POST") {
     res.status(405).json({ message: "Must be POST" });
   } else {
-    // find user
     const user = await db
       .collection("users")
       .findOne({ email: req.body.email });
-    // const uid = user._id;
-    // console.log(uid);
+
     if (!user) {
       res.status(400).json({ message: "Account not found." });
     } else {
       // check if hashed password matches the one in db
-      compare(req.body.password, user.password, function (err, result) {
+      compare(req.body.password, user.password, function(err, result) {
         if (!err && result) {
           const claims = { email: req.body.email, uid: user._id };
           const jwt = sign(claims, process.env.JWT_SECRET, { expiresIn: "1h" });
