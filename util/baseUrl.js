4eb19129adf05db50e025b182bcf47f16a72eeba
Sanojan Thiyagaraja <sanojan99@gmail.com>
2021-05-28 19:50:49 +1000

    updated baseUrl

--- util/baseUrl.js
+++ util/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://resonance-snuzzn.vercel.app";
+const baseUrl = "https://resonance-bookmarks.vercel.app";
 // const baseUrl = "http://localhost:3000";
 
 export default baseUrl;
